1dbfd4214adc08a2a269ffca9f89e8efe0a6b520
Alex Fedoseev <alex@fedoseev.mx>
2022-02-12 22:17:36 +0300

    Add nix shell

---
+++ shell.nix
@@ -0,0 +1,14 @@
+with (import <nixpkgs> {});
+
+mkShell {
+  buildInputs = [
+    nodejs-16_x
+    go_1_17
+    libiconv
+    darwin.apple_sdk.frameworks.Security
+  ];
+
+  shellHook = ''
+    export PATH="$PWD/node_modules/.bin/:$PATH";
+  '';
+}
